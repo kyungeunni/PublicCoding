9702a60353a55839f4f16eca3458f9532e8b00e5
sungwon jung <poloya2000@naver.com>
2016-04-18 11:33:04 +0900

    login 추가 / login 결과에 출력화면은 재설정해야함

---
+++ PublicCoding/src/main/java/com/puco/board/MemberController.java
@@ -0,0 +1,36 @@
+package com.puco.board;
+
+import java.util.StringTokenizer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.puco.controller.Controller;
+import com.puco.controller.RequestMapping;
+import com.puco.member.dao.MemberDAO;
+
+@Controller("mc")
+public class MemberController {
+	@RequestMapping("signin.do")
+	public String memberSignin(HttpServletRequest req) {
+		String id=req.getParameter("id");
+		System.out.println(id);
+		String pwd=req.getParameter("pwd");
+		System.out.println(pwd);
+		// DB연동
+		String res = MemberDAO.memberLogin(id, pwd);
+		String email="";
+		if(!(res.equals("NOID")||res.equals("NOPWD"))) {
+			// 로그인 결과로 MemberDAO에서 email만 받아오기 때문에 따로 잘라내지 않았다.
+			StringTokenizer st = new StringTokenizer(res, "|");
+			email=st.nextToken();
+			HttpSession session=req.getSession();
+			session.setAttribute("id", id);
+			session.setAttribute("pwd", pwd);
+			session.setAttribute("email", email);
+		}
+		req.setAttribute("res", res);
+		System.out.println("MemberController -> login.jsp");
+		return "member/login.jsp";
+	}
+}
---
+++ PublicCoding/src/main/webapp/member/login.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!-- /////////////signin from MemberController///////////// -->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:choose>
+  <c:when test="${res=='NOID' }">
+   <script>
+   alert("ID가 존재하지 않습니다");
+   history.back();
+   </script>
+  </c:when>
+  <c:when test="${res=='NOPWD' }">
+   <script>
+   alert("비밀번호가 틀립니다");
+   history.back();
+   </script>
+  </c:when>
+  <c:otherwise>
+    <c:redirect url="../signin.do"/>
+  </c:otherwise>
+</c:choose>
+<!-- ////////////////////////////////////////////////////// -->
