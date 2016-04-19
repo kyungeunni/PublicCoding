259d1f4b133c4efec44997fc4619045079589c8e
sungwon jung <poloya2000@naver.com>
2016-04-19 18:03:36 +0900

    스마트에디터 버튼의 기능부 추가(테스트 필요함)

--- PublicCoding/src/main/webapp/resources/se/js/hp_SE_CodeInsert.js
+++ PublicCoding/src/main/webapp/resources/se/js/hp_SE_CodeInsert.js
@@ -1,28 +1,32 @@
 /*
  *	스마트에디터 코드 추가 버튼 기능 구현부 (CodeInsert)
  */
+
 nhn.husky.SE_CodeInsert = jindo.$Class({
-    name : "CodeInsert",
-    
-	$init : function(elAppContainer) {
-		this._assignHTMLObjects(elSppContainer);
+    name : "SE_CodeInsert",
+    $init : function(elAppContainer){
+		this._assignHTMLObjects(elAppContainer);
 	},
 	
 	_assignHTMLObjects : function(elAppContainer){
 	    this.oDropdownLayer =
 	            cssquery.getSingle("DIV.husky_seditor_CodeInsert_layer", elAppContainer);
+	    //div 레이어 안에 있는 input button을 cssquery로 찾는 부분.
+	    this.oInputButton = cssquery.getSingle(".se_button_code", elAppContainer);
 	},
 	
 	$ON_MSG_APP_READY : function() {
-	    this.oApp.exec('REGISTER_UI_EVENT', ['CodeInsert', 'click',
-	            'SE_TOGGLE_CODEINSERT_LAYER']);
+	    this.oApp.exec('REGISTER_UI_EVENT',
+	    		["CodeInsert", "click", "SE_TOGGLE_CODEINSERT_LAYER"]);
+	    // input button에 click 이벤트를 할당
+	    this.oApp.registerBrowserEvent(this.oInputButton, 'click', 'PASTE_CODE_BACK');
 	},
 	
-	$ON_SE_TOGGLE_TIMESTAMPER_LAYER : function(){
+	$ON_SE_TOGGLE_CODEINSERT_LAYER : function(){
         this.oApp.exec("TOGGLE_TOOLBAR_ACTIVE_LAYER", [this.oDropdownLayer]);
     },
  
-    $ON_PASTE_NOW_DATE : function(){
-        this.oApp.exec("PASTE_HTML", [new Date()]);
+    $ON_PASTE_CODE_BACK : function(){
+        this.oApp.exec("PASTE_HTML", ["<pre><code> </code></pre>"]);
     }
 });
