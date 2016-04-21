325b28ee80215cfc66013766554f74570c5d11cf
sungwon jung <poloya2000@naver.com>
2016-04-21 11:49:30 +0900

    URL 파서기 테스트 중간저장

--- PublicCoding/src/main/java/com/puco/lectures/dao/VideoChange.java
+++ PublicCoding/src/main/java/com/puco/lectures/dao/VideoChange.java
@@ -10,7 +10,9 @@ public class VideoChange {
     <a href="videolist.do?mode=8" class="list-group-item">-CSS-</a>
     <a href="videolist.do?mode=9" class="list-group-item">-JavaScript-</a>*/
 	private static String[] url={
-			 "https://www.youtube.com/results?q=%EC%9E%90%EB%B0%94+%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D&sp=EgIQAw%253D%253D",//홈
+			 //"https://www.youtube.com/watch?v=qR90tdW0Hbo&list=PLuHgQVnccGMCeAy-2-llhw3nWoQKUvQck" // 생활코딩 자바,
+			 "https://www.youtube.com/watch?v=PZIPsKgWJiw&list=PLuHgQVnccGMA4uSig3hCjl7wTDeyIeZVU" // 생활코딩 자바스크립트
+/*			 "https://www.youtube.com/results?q=%EC%9E%90%EB%B0%94+%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D&sp=EgIQAw%253D%253D",//홈
 			 "https://www.youtube.com/results?q=%EC%9E%90%EB%B0%94+%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D&sp=EgIQAw%253D%253D", //자바강의
 			 "https://www.youtube.com/results?q=c+%EC%96%B8%EC%96%B4&sp=EgIQAw%253D%253D",//C/C++
 			 "https://www.youtube.com/results?sp=EgIQAw%253D%253D&q=JSP+%EA%B0%95%EC%A2%8C",//JSP
@@ -19,7 +21,7 @@ public class VideoChange {
 			 "https://www.youtube.com/results?q=html5+%EA%B0%95%EC%A2%8C&sp=EgIQAw%253D%253D",//Html
 			 "https://www.youtube.com/results?sp=EgIQAw%253D%253D&q=css3+%EA%B0%95%EC%A2%8C",//CSS
 			 "https://www.youtube.com/results?sp=EgIQAw%253D%253D&q=%EC%9E%90%EB%B0%94+%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8"//JavaScript
-	   };
+*/	   };
 	   public static String change(int no){
 		   return url[no];
 	   }
--- PublicCoding/src/main/java/com/puco/lectures/dao/VideoDAO.java
+++ PublicCoding/src/main/java/com/puco/lectures/dao/VideoDAO.java
@@ -21,9 +21,12 @@ public class VideoDAO {
 		try{
 			Document doc=Jsoup.connect(modeURL).get();
 			//System.out.println(doc);
-			Elements titleElem=doc.select("a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2");
+			/*Elements titleElem=doc.select("a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2");
 			Elements imageElem=doc.select("span.yt-thumb-simple img");
-			Elements urlElem=doc.select("a.yt-uix-sessionlink.yt-pl-thumb-link");
+			Elements urlElem=doc.select("a.yt-uix-sessionlink.yt-pl-thumb-link");*/
+			Elements titleElem=doc.select("h4.yt-ui-ellipsis.yt-ui-ellipsis-2");
+			Elements imageElem=doc.select("span.yt-thumb-clip img");
+			Elements urlElem=doc.select("a.yt-uix-sessionlink.spf-link.playlist-video.clearfix.spf-link");
 			//배트맨 대 슈퍼맨: 저스티스의 시작 81.1% 2016.03.24 개봉 24,348 74% http://img.cgv.co.kr/Movie/Thumbnail/Poster/000078/78391/78391_185.jpg No.1 12세 이상
 			for(int i=0;i<titleElem.size();i++){
 				Element telem=titleElem.get(i);
@@ -33,6 +36,7 @@ public class VideoDAO {
 				String img=ielem.attr("src");
 				if(img.substring(0,5).equals("https")){
 				img=ielem.attr("data-thumb");
+				System.out.println((i+1)+"번째 title "+telem.text()+" Image "+img+" 강의URL "+url);
 				}
 				VideoDTO d=new VideoDTO();
 				d.setNo(i+1);
