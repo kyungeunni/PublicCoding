37a190033b7064d330534c2f23567351e1bcd4c8
Jungsun Stella Yoon <Jungsun Stella Yoon@StellaYoon>
2016-04-22 10:53:53 +0900

    20160422

---
+++ PublicCoding/src/main/java/com/puco/board/dao/FreeBoardVO.java
@@ -0,0 +1,69 @@
+package com.puco.board.dao;
+
+import java.util.Date;
+
+public class FreeBoardVO {
+	private int no;
+	private String name;
+	private String subject;
+	private String content;
+	private String pwd;
+	private Date regdate;
+	private int hit;
+	private int replyCount;
+	private String drbday;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getReplyCount() {
+		return replyCount;
+	}
+	public void setReplyCount(int replyCount) {
+		this.replyCount = replyCount;
+	}
+	public String getDrbday() {
+		return drbday;
+	}
+	public void setDrbday(String drbday) {
+		this.drbday = drbday;
+	}
+}
---
+++ PublicCoding/src/main/java/com/puco/board/dao/ReplyVO.java
@@ -0,0 +1,90 @@
+package com.puco.board.dao;
+
+import java.util.Date;
+
+public class ReplyVO {
+	private int no;
+	private String id;
+	private String name;
+	private int bno;
+	private String msg;
+	private Date regdate;
+	private int group_id;
+	private int group_step;
+	private int group_tab;
+	private int root;
+	private int depth;
+	private String dbday;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getGroup_id() {
+		return group_id;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	public int getGroup_step() {
+		return group_step;
+	}
+	public void setGroup_step(int group_step) {
+		this.group_step = group_step;
+	}
+	public int getGroup_tab() {
+		return group_tab;
+	}
+	public void setGroup_tab(int group_tab) {
+		this.group_tab = group_tab;
+	}
+	public int getRoot() {
+		return root;
+	}
+	public void setRoot(int root) {
+		this.root = root;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public String getDbday() {
+		return dbday;
+	}
+	public void setDbday(String dbday) {
+		this.dbday = dbday;
+	}
+}
